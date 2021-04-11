d53045a0802f7ade3cabdce649464d446353fec9
ZackJorquera <jorquerazack@gmail.com>
2021-04-11 09:53:01 -0600

    added graph_part code

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import numpy as np
 
 
+def data_to_adj_mat(time_series_data):
+    pass
+
+
 def page_rank():
     pass
 
 
-def graph_part():
-    pass
+def graph_part(adj_mat):
+    D = np.diag(np.sum(adj_mat, axis=0))
+    L = D - adj_mat
+    eig_vals, eig_vecs = np.linalg.eigh(L)
+    p = np.argsort(eig_vals)
+
+    fiedler = np.array(eig_vecs[:, p[1]]).ravel()
+    perm = np.argsort(fiedler)
+
+    return fiedler, perm
 
 
 if __name__ == '__main__':
